db2f95d707af7d7a2ac8916f27bc5730f1d2928b
matorolll <mgajos00@gmail.com>
2023-07-10 09:08:20 +0200

    Add files via upload

---
+++ Data/Context.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCFullStack.Data
+
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context>options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Action", DisplayOrder = 1},
+                new Category { CategoryId = 2, Name = "SciFi", DisplayOrder = 2},
+                new Category { CategoryId = 3, Name = "History", DisplayOrder = 3},
+                new Category { CategoryId = 4, Name = "Comedy", DisplayOrder = 4}
+                );
+        }
+    }
+}
