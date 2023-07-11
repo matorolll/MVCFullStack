4954aec8b86c7ac94e44619e2c64f1e50f199cdd
matorolll <mgajos00@gmail.com>
2023-07-11 09:28:19 +0200

    Add files via upload

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -14,6 +14,8 @@ namespace MVCFullStack.Controllers
 			List<Category> objCategoryList = _db.Categories.ToList();
 			return View(objCategoryList);
 		}
+
+
 		public IActionResult Create()
 		{
 			return View();
@@ -28,6 +30,7 @@ namespace MVCFullStack.Controllers
 			{
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 			}
 			return View();
@@ -41,30 +44,63 @@ namespace MVCFullStack.Controllers
 				return NotFound();
 			}
 			Category? categoryFromDb = _db.Categories.Find(id);
-			Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id);
-			Category? categoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();
+			//Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u => u.CategoryId == id);
+			//Category? categoryFromDb2 = _db.Categories.Where(u => u.CategoryId == id).FirstOrDefault();
 
 
 			if(categoryFromDb == null)
 			{
 				return NotFound();
 			}
-			return View();
+			return View(categoryFromDb);
 		}
+
+
 		[HttpPost]
 		public IActionResult Edit(Category obj)
 		{
-			if (obj.Name == obj.DisplayOrder.ToString())
-			{
-				ModelState.AddModelError("name", "DisplayOrder cannot exactly match the name");
-			}
 			if (ModelState.IsValid)
 			{
-				_db.Categories.Add(obj);
+				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index");
 			}
 			return View();
 		}
+
+
+
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? categoryFromDb = _db.Categories.Find(id);
+
+			if (categoryFromDb == null)
+			{
+				return NotFound();
+			}
+			return View(categoryFromDb);
+		}
+
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? obj = _db.Categories.Find(id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
+			return RedirectToAction("Index");
+		}
+
 	}
 }
